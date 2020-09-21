a0050c92b2213d08926e8132aa51b071c3b8efbd
AugustoCR13 <65315211+AugustoCR13@users.noreply.github.com>
2020-09-20 21:16:16 -0300

    re-design

--- scripts.js
+++ scripts.js
@@ -47,35 +47,33 @@ function confirma(){
     switch (variavel) {
         case 1: 
             a = valor;
-            A.innerHTML =`A: ${a}`;
-            B.innerHTML =`B: ${b}`;
-            C.innerHTML =`C: ${c}`;
-            Delta.innerHTML =`Delta: ${delta}`;
-            X1.innerHTML =`X1: ${x1}`;
-            X2.innerHTML =`X: ${x2}`;
+            A.innerHTML =`${a} X² +`;
+            /*Delta.innerHTML =`Delta: ${delta}`;
+            X1.innerHTML =`${x1}`;
+            X2.innerHTML =`${x2}`;*/
             Fala.innerHTML= `Insira a variável B`;
         break;
         case 2:
             b = valor;
-            B.innerHTML =`B: ${b}`;
+            B.innerHTML =`${b} X +`;
             Fala.innerHTML= `Insira a variável C`;
         break;
         case 3:
             c = valor;
-            C.innerHTML =`C: ${c}`;
+            C.innerHTML =`${c} = 0`;
             delta = Math.pow(b,2)-4*a*c;
             x1 = (-b + Math.sqrt(delta))/(2*a);
             x2 = (-b - Math.sqrt(delta))/(2*a);
 
-            Delta.innerHTML =`Delta: ${delta}`;
-            X1.innerHTML =`X1: ${x1}`;
-            X2.innerHTML =`X: ${x2}`;
+            Delta.innerHTML =`Δ = ${delta}`;
+            X1.innerHTML =`X = ${x1}`;
+            X2.innerHTML =`X'' = ${x2}`;
             a=0; b=0; c=0; delta=0; x1=0; x2=0; variavel=0;
             Fala.innerHTML= `Insira a variável A`;
         break;
     }
     valor = 0;
-    Exibe.innerHTML=` ${valor}`;
+    Exibe.innerHTML=`${valor}`;
     variavel++
 }
 function apagar(){
